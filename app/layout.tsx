231d24bfac37864b258a46578a99b3b916ebda22
DevGisoun <devgisoun@gmail.com>
2025-09-19 08:41:21 +0000

    [feat] 메타데이터 추가 및 기본 제목 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
-import "./globals.css";
+import type { Metadata } from 'next';
+
+// CSS
+import './globals.css';
+
+const defautTitle = 'Dev. Gisoun - Portfolio';
+
+export const metadata: Metadata = {
+    title: {
+        default: defautTitle,
+        template: `%s | ${defautTitle}`,
+    },
+};
 
 export default function RootLayout({
     children,
